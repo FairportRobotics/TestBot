5afc09b2ebb067144b8817f358edab3fe7e187f3
Administrator <tywilly300@gmail.com>
2014-04-12 10:18:13 -0400

    Added Tyler's subsystem and command.

--- src/com/fairportfirst/test/commands/TylersCommand.java
+++ src/com/fairportfirst/test/commands/TylersCommand.java
@@ -1,13 +1,16 @@
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.fairportfirst.test.commands;
 
 /**
  *
- * @author bradmiller
+ * @author Tyler
  */
-public class ExampleCommand extends CommandBase {
-
-    public ExampleCommand() {
+public class TylersCommand extends CommandBase {
+    
+    public TylersCommand() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
