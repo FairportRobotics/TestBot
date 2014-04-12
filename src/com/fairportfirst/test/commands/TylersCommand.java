c15247ff7903e8d7578508068fba331e2f524e79
Administrator <tywilly300@gmail.com>
2014-04-12 10:36:18 -0400

    Added one motor drive.

--- src/com/fairportfirst/test/commands/TylersCommand.java
+++ src/com/fairportfirst/test/commands/TylersCommand.java
@@ -13,6 +13,7 @@ public class TylersCommand extends CommandBase {
     public TylersCommand() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+        requires(tylerSubsystem);
     }
 
     // Called just before this Command runs the first time
@@ -21,6 +22,7 @@ public class TylersCommand extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        tylerSubsystem.drive(1);
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/com/fairportfirst/test/subsystems/TylersSubsystem.java
+++ src/com/fairportfirst/test/subsystems/TylersSubsystem.java
@@ -4,6 +4,10 @@
  */
 package com.fairportfirst.test.subsystems;
 
+import com.fairportfirst.test.RobotMap;
+import com.fairportfirst.test.commands.TylersCommand;
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -14,8 +18,27 @@ public class TylersSubsystem extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+    CANJaguar driveOne;
+    
+    public TylersSubsystem(){
+        try {
+            driveOne = new CANJaguar(RobotMap.DRIVE_ONE_ID);
+        } catch (CANTimeoutException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new TylersCommand());
+    }
+    
+    public void drive(int speed){
+        try {
+            driveOne.setX(speed);
+        } catch (CANTimeoutException ex) {
+            ex.printStackTrace();
+        }
     }
 }
